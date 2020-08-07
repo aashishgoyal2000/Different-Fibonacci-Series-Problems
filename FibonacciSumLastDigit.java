a18478c31adf090569051bd2b2a185596e68781e
aashishgoyal2000 <54860971+aashishgoyal2000@users.noreply.github.com>
2020-08-07 19:58:32 +0530

    Last Digit Of Sum Of all fibonacci upto n

---
+++ FibonacciSumLastDigit.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class FibonacciSumLastDigit {
+    private static long getFibonacciSumNaive(long n) {
+        if (n <= 1)
+            return n;
+
+        long previous = 0;
+        long current  = 1;
+        long sum      = 1;
+
+        for (long i = 0; i < n - 1; ++i) {
+            long tmp_previous = previous%10;
+            previous = current%10;
+            current = (tmp_previous + current)%10;
+            sum += current;
+            sum = sum % 10;
+            // System.out.println(current + " " + sum);
+        }
+
+        return sum % 10;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        // long s = getFibonacciSumNaive(n);
+        long mod = getFibonacciSumNaive(n%60);
+        System.out.println(mod);
+    }
+}
+
